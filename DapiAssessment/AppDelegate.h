e787524d532f944e89e47544e838a0e18c427912
wefky <minawefky3@gmail.com>
2021-02-17 15:38:52 +0200

    Initial Commit

---
+++ DapiAssessment/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  DapiAssessment
+//
+//  Created by mina wefky on 17/02/2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ DapiAssessment/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  DapiAssessment
+//
+//  Created by mina wefky on 17/02/2021.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
