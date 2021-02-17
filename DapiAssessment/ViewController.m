8c546757ac575fc2d0f3ea79044d0a39ac0d22ec
wefky <minawefky3@gmail.com>
2021-02-17 18:34:22 +0200

    adding serialized data tasks in background thread

--- DapiAssessment/ViewController.m
+++ DapiAssessment/ViewController.m
@@ -33,8 +33,61 @@ NSArray *urls;
             @"boomsupersonic.com",
             @"twitter.com", nil];
     
-
+    [self initingNavBar];
 }
+
+
+- (void)initingNavBar{
+    self.navigationController.navigationBar.prefersLargeTitles = YES;
+    self.title = @"Sites";
+    UIBarButtonItem *startBtn = [[UIBarButtonItem alloc]initWithTitle:@"Start" style:UIBarButtonItemStylePlain target:self action:@selector(startBtnTapped:)];
+    
+    self.navigationItem.rightBarButtonItem = startBtn;
+}
+
+//MARK:- startBtn Tapped
+
+-(IBAction)startBtnTapped:(id)sender
+{
+    [self createGetReusts:0];
+}
+
+
+//MARK:- performe serialBackgroundTask
+- (void)createGetReusts:(int)requstIndex{
+    //data task does not need any GCD cause it runs on diffrent thread as it has complition block
+    NSString *baseURL = @"http://www.";
+    NSURLSessionDataTask *dataTask;
+    NSURL *url = [NSURL URLWithString:[baseURL stringByAppendingString:urls[requstIndex]]];
+    dataTask =
+    [[[self class] session] dataTaskWithURL:url
+                          completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        
+        // recursive way serialize the requests
+        if (requstIndex< [urls count] -1){
+            [self createGetReusts:requstIndex + 1];
+        }
+    }];
+    NSLog(@"%@", dataTask);
+    [dataTask resume];
+}
+
++ (NSURLSession *)session
+{
+    static NSURLSession *session = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        
+        NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+        
+        [configuration setHTTPMaximumConnectionsPerHost:1];
+        
+        session = [NSURLSession sessionWithConfiguration:configuration];
+        
+    });
+    return session;
+}
+
 
 //MARK:- Table View Data source
 
